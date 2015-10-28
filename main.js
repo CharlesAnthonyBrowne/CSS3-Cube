5e224a8f6032c8df3535b0088407b0fce4710ce7
CharlesAnthonyBrowne <charles@charlesbrowne.io>
2015-10-28 12:58:06 +0000

    css changes and smooth transition via class in js

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 (function mainJS() {
-  
-});
+  var btn = document.getElementsByClassName('btn_img');
+  var container = document.getElementById('design-wrapper');
+
+  btn[0].addEventListener('click', function() {
+    console.log(container);
+    container.className += 'split_anim';
+  });
+})();
