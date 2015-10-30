0fa4153dfd5cb90f470833d0554e4169662c80ef
CharlesAnthonyBrowne <charles@charlesbrowne.io>
2015-10-30 15:04:29 +0000

    changes

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 (function mainJS() {
   var btn = document.getElementsByClassName('btn_img');
+
   var container = document.getElementById('design-wrapper');
+  var top = document.getElementsByClassName('content');
+  var bottom = document.getElementsByClassName('bottom');
 
   btn[0].addEventListener('click', function() {
-    console.log(container);
-    container.className += 'split_anim';
+    top[0].className += ' split_anim';
+    container.className += 'top_anim';
+    bottom[0].className += ' bottom_anim';
   });
 })();
