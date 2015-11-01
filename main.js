ba0366fafadfe6bf6f0927b4e5a45606d56ab0aa
CharlesAnthonyBrowne <charles@charlesbrowne.io>
2015-11-01 15:57:58 +0000

    changed the onClick

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
   var inner = document.getElementsByClassName('inner');
 
   btn.addEventListener('click', function(){
-    inner[0].style.transform = "rotate(0)";
+    inner[0].style.transform = "rotateY(40deg)";
     console.log(inner[0]);
   });
 })();
