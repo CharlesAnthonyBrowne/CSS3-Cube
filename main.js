13e7af38a1d4fdaa76242f067358117a43bcd594
CharlesAnthonyBrowne <charles@charlesbrowne.io>
2015-11-06 16:45:05 +0000

    working onClick event --need to add toggle

--- main.js
+++ main.js
@@ -2,14 +2,18 @@
   var btn = document.getElementById('toggle_btn');
   var cube = document.getElementById('cube');
 
-  btn.addEventListener('click', function(){
-    if (cube.className = "animate") {
-        cube.className = "";
-      }
-      else {
-        cube.className = "animate";
-      }
+  btn.addEventListener('click', function() {
+    cube.className = "animate";
   });
+
+  // btn.addEventListener('click', function(){
+  //   if (cube.className = "animate") {
+  //       cube.className = "";
+  //     }
+  //     else {
+  //       cube.className = "animate";
+  //     }
+  // });
 
 
 })();
